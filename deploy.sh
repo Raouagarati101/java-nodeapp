1d055f8e1d99c1e33ca416d63fe58332d240a6f0
Raouagarati101 <32985965+Raouagarati101@users.noreply.github.com>
2020-06-06 15:14:53 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 # This is to demo
 
-node_app=`docker ps -a | grep nodeapp | awk '{print $NF}'`
-if [ $node_app=='nodeapp' ]; then
+node_app=`docker ps -a | grep java-nodeapp | awk '{print $NF}'`
+if [ $node_app=='java-nodeapp' ]; then
     echo "nodeapp is running, lets delete"
-        docker rm -f nodeapp
+        docker rm -f java-nodeapp
 fi
 
-images=`docker images | grep kammana/nodejenkins | awk '{print $3}'`
+images=`docker images | grep raouagara/java-nodeapp | awk '{print $3}'`
 docker rmi $images
-docker run -d -p 8080:8080 --name nodeapp $1
+docker run -d -p 8080:8080 --name java-nodeapp $1
